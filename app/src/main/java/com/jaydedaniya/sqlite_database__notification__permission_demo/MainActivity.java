ed2dd706d05c481902d6b0213df1392b3dd785a4
Jay Dedaniya <jay.07.dedaniya@gmail.com>
2022-07-23 17:58:36 +0530

    Code added to Splash Screen

--- app/src/main/java/com/jaydedaniya/sqlite_database__notification__permission_demo/MainActivity.java
+++ app/src/main/java/com/jaydedaniya/sqlite_database__notification__permission_demo/MainActivity.java
@@ -2,13 +2,40 @@ package com.jaydedaniya.sqlite_database__notification__permission_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.splashScreenProgressBar);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <= 100; i++) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }).start();
+
     }
 }
